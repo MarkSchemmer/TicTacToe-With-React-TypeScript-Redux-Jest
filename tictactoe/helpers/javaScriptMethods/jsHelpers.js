568e27d8a17ddc856a794eb2e8440ae58c147172
Ramesh Maganti (Zen3 Infosolutions America Inc) <v-ramaga@microsoft.com>
2019-02-09 18:36:16 -0800

    added types and js helper methods, as well I updated my notes

---
+++ tictactoe/helpers/javaScriptMethods/jsHelpers.js
@@ -0,0 +1,95 @@
+import * as types from "../types/types";
+
+
+export const OPTIONS = {
+    XWIN : 1,
+    XLOSE : 2,
+    TIE : 3, 
+    MOREMOVES : 4
+}
+
+
+let threeInRow = (chr, [a,b,c], nb) =>  nb[a] === nb[b] && nb[b] === nb[c] && nb[a] === chr
+
+export function IsWinner (board){
+
+    let nb = board
+    .reduce((acc,cur) => acc.concat(cur), [])
+    .map( x => x.val )
+
+    let waysToWin = [
+        [0,1,2],
+        [0,4,8],
+        [0,3,6],
+        [1,4,7],
+        [2,4,6],
+        [2,5,8],
+        [3,4,5],
+        [6,7,8]
+    ]
+
+    for(let i = 0; i < waysToWin.length; i++) {
+
+        if(threeInRow('X', waysToWin[i], nb))
+            return OPTIONS.XWIN
+        else if (threeInRow('O', waysToWin[i], nb))
+            return OPTIONS.XLOSE
+    }
+
+    return nb.every(x => x !== null ) ? OPTIONS.TIE : OPTIONS.MOREMOVES
+}
+
+
+export function getWinningSquares (board) {
+    let nb = board
+    .reduce((acc,cur) => acc.concat(cur), [])
+    .map( x => x )
+
+    let _nb = board
+    .reduce((acc,cur) => acc.concat(cur), [])
+    .map( x => x.val )
+
+    let waysToWin = [
+        [0,1,2],
+        [0,4,8],
+        [0,3,6],
+        [1,4,7],
+        [2,4,6],
+        [2,5,8],
+        [3,4,5],
+        [6,7,8]
+    ]
+
+    for(let i = 0; i < waysToWin.length; i++) {
+        
+
+        let [a,b,c] = waysToWin[i]
+
+
+        if(threeInRow('X', waysToWin[i], _nb)){
+            console.log(nb)
+            return [a,b,c].map(x => nb[x].coor)
+        }
+        else if (threeInRow('O', waysToWin[i], _nb)){
+            console.log(nb)
+            return [a,b,c].map(x => nb[x].coor)
+        }
+           
+    }
+
+    return []
+}
+
+
+export function genBoard () {
+    let id = 0
+    let square = types.Square
+    let board = [...Array(3).keys()].map((val, x) => {
+        return [...Array(3).keys()].map((innerVal, y) => {
+            return new square(x,y, id++);
+        })
+    })
+
+
+    return board 
+}
---
+++ tictactoe/helpers/types/types.ts
@@ -0,0 +1,49 @@
+import * as jsHelpers from "../javaScriptMethods/jsHelpers";
+
+
+interface ISquare {
+    id : number;
+    Value : string | null;
+    Coordinate : Array<number>;
+}
+
+interface IMove {
+    Move : Array<number> | null;
+    Board : Array<Array<Square>>
+}
+
+interface IState {
+    Turn : number;
+    History : Array<Move>;
+}
+
+
+export class Square implements ISquare {
+    public Value : string | null;
+    public Coordinate : Array<number>;
+    public id : number;
+    constructor(x:number, y:number, id:number){
+        this.Value = null; 
+        this.Coordinate  = [x,y];
+        this.id = id;
+    }
+}
+
+export class Move implements IMove {
+    public Move : Array<number> | null;
+    public Board : Array<Array<Square>>;
+    constructor(Board:Array<Array<Square>>, Move:Array<number>=[]){
+        this.Board = Board;
+        this.Move = Move;
+    }
+}
+
+export class State implements IState {
+    public History : Array<Move>
+    public Turn : number 
+    constructor(){
+        this.Turn = 0; 
+        this.History = [new Move(jsHelpers.genBoard())];
+    }
+}
+
